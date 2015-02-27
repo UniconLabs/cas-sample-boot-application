94479cc870f519ddff28a19089c8c10e4687601a
Jj <jsjohnson@unicon.net>
2015-02-27 16:32:18 -0600

    committing lingering code

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ Parameter | Notes
 server.port | Port to run the application on. default is `8888`
 local.serverName | Server name used for CAS. default is `http://localhost:8888`
 cas.serverUrlPrefix | Server prefix for the CAS server. Default is `https://test.scaldingspoon.org/cas`
+spring.profiles.active | what profile to use. This mostly determines what protocol to use for CAS authentication and validation. Valid options are `saml` and `cas`. Default is `saml`.
--- src/main/groovy/net/unicon/boot/test/TestApplication.groovy
+++ src/main/groovy/net/unicon/boot/test/TestApplication.groovy
@@ -1,19 +1,21 @@
 package net.unicon.boot.test
 
 import org.jasig.cas.client.authentication.AttributePrincipal
+import org.jasig.cas.client.authentication.AuthenticationFilter
 import org.jasig.cas.client.authentication.Saml11AuthenticationFilter
 import org.jasig.cas.client.util.AssertionThreadLocalFilter
 import org.jasig.cas.client.util.HttpServletRequestWrapperFilter
+import org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter
 import org.jasig.cas.client.validation.Saml11TicketValidationFilter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.context.embedded.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 import javax.servlet.http.HttpServletRequest
 
@@ -27,7 +29,22 @@ class TestApplication {
     String casServerUrlPrefix
 
     @Bean
-    FilterRegistrationBean authenticationFilter() {
+    @Profile('cas')
+    FilterRegistrationBean casAuthenticationFilter() {
+        new FilterRegistrationBean(
+                filter: new AuthenticationFilter(),
+                urlPatterns: ['/*'],
+                order: 1,
+                initParameters: [
+                        'casServerLoginUrl': "${casServerUrlPrefix}/login".toString(),
+                        'serverName'       : localServerName
+                ]
+        )
+    }
+
+    @Bean
+    @Profile('saml')
+    FilterRegistrationBean samlAuthenticationFilter() {
         new FilterRegistrationBean(
                 filter: new Saml11AuthenticationFilter(),
                 urlPatterns: ['/*'],
@@ -40,7 +57,21 @@ class TestApplication {
     }
 
     @Bean
-    FilterRegistrationBean validationFilter() {
+    FilterRegistrationBean casValidationFilter() {
+        new FilterRegistrationBean(
+                filter: new Cas20ProxyReceivingTicketValidationFilter(),
+                urlPatterns: ['/*'],
+                order: 2,
+                initParameters: [
+                        'casServerUrlPrefix': casServerUrlPrefix,
+                        'serverName'        : localServerName
+                ]
+        )
+        
+    }
+
+    @Bean
+    FilterRegistrationBean samlValidationFilter() {
         new FilterRegistrationBean(
                 filter: new Saml11TicketValidationFilter(),
                 urlPatterns: ['/*'],
